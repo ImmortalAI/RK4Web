8fbe01286ba1e1f5e1103100ccd5c6f6aade9477
ImmortalAI <moniki2139@yandex.ru>
2025-05-07 19:42:20 +0300

    add vue wrapper for mathlive

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import MathLiveInput from './components/MathLiveInput.vue'
+
+const latex = ref('\\lambda=a+b')
+</script>
 
 <template>
   <header>
     <h1 class="text-3xl font-bold underline">Hello</h1>
   </header>
 
-  <main></main>
+  <main class="flex flex-col justify-center items-center h-screen">
+    <MathLiveInput class="min-w-xs" v-model="latex" />
+  </main>
 </template>
